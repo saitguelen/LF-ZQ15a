62e47a3cd0fb42351fd819e73d4367dfe4f69941
saitguelen <saidgln25@gmail.com>
2025-04-01 09:41:57 +0200

    Fünfte Committ

--- Tag2/Frage1.py
+++ Tag2/Frage1.py
@@ -46,7 +46,67 @@ def func(d,key, value):
 
 print(func(data,'1','Peter')) #Ausgabe: None
 print(func(data,'2','Patrik'))
+
+===============================>
+x="2"
+y=2*x
+print(y)
+=============================>5. Frage
+
+
+
+my_list = [x * x for x in range(5)]
+
+
+def fun(lst):
+    del lst[lst[2]]
+    return lst
+
+
+print(fun(my_list))
+
+#Tryityourself:
+
+my_list = [x * x for x in range(5)]
+print(my_list)  # [0, 1, 4, 9, 16]
+# The same without list comprehension:
+my_list = []
+for x in range(5):
+    my_list.append(x * x)
+print(my_list)  # [0, 1, 4, 9, 16]
+
+
+def fun(lst):
+    # del lst[lst[2]]
+    del lst[4]
+    return lst
+
+
+print(fun(my_list))  # [0, 1, 4, 9]
+
+
+=============================>6. Frage
+
+x=float('23.42')
+print(bool(x)+True)
+
+#Tryityourself:
+
+x = float('23.42')
+
+print(bool(x) + True)  # 2
+print(int(x) + False)  # 23
+print(str(x))  # '23.42'
+print(bool(x))  # True
+
+print(float('23.42'))  # 23.42
+print(bool(23.42))  # True
+
+print(True + True)  # 2
+print(True - False)  # 1
+print(True * True)  # 1
+print(True / True)  # 1.0
+print(True % True)  # 0
+
 """
-
-
 
